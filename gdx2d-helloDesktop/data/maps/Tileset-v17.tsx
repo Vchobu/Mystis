3e314857ac8c1fe53dc6bf58dabb2b441cc5f1e5
Strix45 <dino.bijelic@students.hevs.ch>
2025-06-11 00:32:43 +0200

    Menu.scala Options.scala ProgressionSystem.scala Music_and_sfx.scala MystisUnifiedApp.scala = launcher + d'autre mofification

---
+++ gdx2d-helloDesktop/data/maps/Tileset-v17.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tileset-v17" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="Tileset-v17.png" trans="000000" width="1024" height="1024"/>
+</tileset>
